3b057a3e847b55b4eff1910560ccc73a4ac8504b
MaratHmit <mp8194d9x>
2016-07-21 18:27:16 +0500

    рефакторинг Auth

--- 2_dev/vendor/SE/Shop/Auth.php
+++ 2_dev/vendor/SE/Shop/Auth.php
@@ -52,11 +52,39 @@ class Auth extends Base
             return array();
     }
 
+    private function getAuthData()
+    {
+        $url = AUTH_SERVER . "/api/2/Auth/Register.api";
+        $ch = curl_init($url);
+        $data["serial"] = $this->input["login"];
+        $data["hash"] = $this->input["hash"];
+        $data["project"] = $this->input["project"];
+        $apiData = json_encode($data);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $apiData);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+                'Content-Type: application/json',
+                'Content-Length: ' . strlen($apiData))
+        );
+        $result = json_decode(curl_exec($ch), 1);
+        if ($result["status"] == "ok")
+            return $result["data"];
+        return null;
+    }
+
+
     public function info()
     {
-        $userToken = md5($this->input["login"] . $this->input["password"]);
+        $authData = $this->getAuthData();
+        if (!$authData) {
+            $this->error = "Проект не найден или не активен!";
+            return null;
+        }
+
         try {
-            if (TOKEN == trim($userToken)) {
+            if ($authData["isAdmin"]) {
                 $data['userDisplay'] = 'Администратор';
                 $data['isAdmin'] = true;
             } else {
@@ -65,12 +93,15 @@ class Auth extends Base
                     CONCAT_WS(" ", p.last_name, CONCAT_WS(".", SUBSTR(p.first_name, 1, 1), SUBSTR(p.sec_name, 1, 1))) displayName');
                 $u->innerJoin('person p', 'p.id=su.id');
                 $u->where('is_active="Y" AND username="?"', $this->input["login"]);
-                $u->andWhere('password="?"', strtolower($this->input["password"]));
+                $u->andWhere('password="?"', strtolower($this->input["hash"]));
                 $result = $u->fetchOne();
                 if (!empty($result)) {
                     $data['userDisplay'] = $result["displayName"];
                     $data['idUser'] = $result["id"];
-                } else $this->error = 'Неправильное имя пользователя или пароль!';
+                } else {
+                    $this->error = 'Неправильное имя пользователя или пароль!';
+                    throw new Exception($this->error);
+                }
             }
 
             if (!empty($this->error))
@@ -100,11 +131,12 @@ class Auth extends Base
                 }
             }
 
+            $data['config'] = $authData;
             $data['permissions'] = $this->getPermission($data['idUser']);
             $data['accounts'] = $this->getAccounts();
             $_SESSION['idUser'] = $data['idUser'];
-            if (empty($error))
-                $_SESSION['isAuth'] = true;
+            $_SESSION['isAuth'] = true;
+            $_SESSION['hostname'] = HOSTNAME;
             $this->result = $data;
 
         } catch (Exception $e) {
