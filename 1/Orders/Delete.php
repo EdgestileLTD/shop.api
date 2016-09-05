9867e0804ad13ea21392525f84cf90f6c2d059ec
MaratHmit <hmm1979@mail.ru>
2016-09-05 18:23:50 +0500

    api v.1 - удаление заказа (перевод в отмененные)

--- 1/Orders/Delete.php
+++ 1/Orders/Delete.php
@@ -1,30 +1,27 @@
 <?php
 
-    $idsStr = implode(",", $json->ids);
-    // ‚ÓÁ‚‡Ú ÚÓ‚‡Ó‚
-    se_db_query("UPDATE shop_price sp
+$idsStr = implode(",", $json->ids);
+se_db_query("UPDATE shop_price sp
                 INNER JOIN shop_tovarorder st ON sp.id = st.id_price
                 SET sp.presence_count = sp.presence_count + st.count
                 WHERE st.id_order IN ({$idsStr}) AND sp.presence_count IS NOT NULL AND sp.presence_count >= 0");
-    // ‚ÓÁ‚‡Ú ÏÓ‰ËÙËÍ‡ˆËÈ
-    se_db_query("UPDATE shop_modifications sm
+se_db_query("UPDATE shop_modifications sm
                     INNER JOIN shop_tovarorder st ON sm.id IN (st.modifications)
                     INNER JOIN shop_price sp ON sp.id = st.id_price
                     SET sm.count = sm.count + st.count
                     WHERE st.id_order IN ({$idsStr}) AND sm.count IS NOT NULL AND sm.count >= 0");
 
 
-    if ($json->ids) {
-        $u = new seTable('shop_order','so');
-        $u->where('id in (?)', $idsStr)->deletelist();
-    }
+if ($json->ids) {
+    se_db_query("UPDATE shop_order SET is_delete = 'Y' WHERE id IN ({$idsStr})");
+}
 
-    $status = array();
-    if (!mysql_error()) {
-        $status['status'] = 'ok';
-    } else {
-        $status['status'] = 'error';
-        $status['errortext'] = mysql_error();
-    }
+$status = array();
+if (!se_db_error()) {
+    $status['status'] = 'ok';
+} else {
+    $status['status'] = 'error';
+    $status['errortext'] = "–ù–µ —É–¥–∞—ë—Ç—Å—è –ø–µ—Ä–µ–≤–µ—Å—Ç–∏ —É–∫–∞–∑–∞–Ω–Ω—ã–µ –∑–∞–∫–∞–∑—ã –≤ —Å—Ç–∞—Ç—É—Å: '–û—Ç–º–µ–Ω–µ–Ω–Ω—ã–µ'";
+}
 
-    outputData($status);
+outputData($status);
--- 1_dev/Orders/Delete.php
+++ 1_dev/Orders/Delete.php
@@ -1,30 +1,27 @@
 <?php
 
-    $idsStr = implode(",", $json->ids);
-    // возврат товаров
-    se_db_query("UPDATE shop_price sp
+$idsStr = implode(",", $json->ids);
+se_db_query("UPDATE shop_price sp
                 INNER JOIN shop_tovarorder st ON sp.id = st.id_price
                 SET sp.presence_count = sp.presence_count + st.count
                 WHERE st.id_order IN ({$idsStr}) AND sp.presence_count IS NOT NULL AND sp.presence_count >= 0");
-    // возврат модификаций
-    se_db_query("UPDATE shop_modifications sm
+se_db_query("UPDATE shop_modifications sm
                     INNER JOIN shop_tovarorder st ON sm.id IN (st.modifications)
                     INNER JOIN shop_price sp ON sp.id = st.id_price
                     SET sm.count = sm.count + st.count
                     WHERE st.id_order IN ({$idsStr}) AND sm.count IS NOT NULL AND sm.count >= 0");
 
 
-    if ($json->ids) {
-        $u = new seTable('shop_order','so');
-        $u->where('id in (?)', $idsStr)->deletelist();
-    }
+if ($json->ids) {
+    se_db_query("UPDATE shop_order SET is_delete = 'Y' WHERE id IN ({$idsStr})");
+}
 
-    $status = array();
-    if (!mysql_error()) {
-        $status['status'] = 'ok';
-    } else {
-        $status['status'] = 'error';
-        $status['errortext'] = mysql_error();
-    }
+$status = array();
+if (!se_db_error()) {
+    $status['status'] = 'ok';
+} else {
+    $status['status'] = 'error';
+    $status['errortext'] = "Не удаётся перевести указанные заказы в статус: 'Отмененные'";
+}
 
-    outputData($status);
+outputData($status);
