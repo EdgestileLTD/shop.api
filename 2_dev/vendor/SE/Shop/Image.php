c554d72b9a09cba359727109ec94110abdcc8e65
MaratHmit <hmm19792@gmail.com>
2018-02-17 23:44:14 +0300

    API v.2 - удаление неиспользуемых изображений - добавлена проверка на присутствие изображений в описаниях

--- 2_dev/vendor/SE/Shop/Image.php
+++ 2_dev/vendor/SE/Shop/Image.php
@@ -86,11 +86,30 @@ class Image extends Base
 
         if ($this->section == 'shopprice' && $isUnused) {
             $u = new DB('shop_price', 'sp');
-            $u->select('img name');
-            $images = $u->getList();
-            foreach ($images as $image)
-                if ($image['name'])
-                    $usedImages[] = $image['name'];
+            $u->select('img name, note, text');
+            $products = $u->getList();
+            foreach ($products as $product) {
+                if ($product['name'])
+                    $usedImages[] = $product['name'];
+
+                $media = [];
+                if ($product["note"]) {
+                    preg_match_all('/<img(?:\\s[^<>]*?)?\\bsrc\\s*=\\s*(?|"([^"]*)"|\'([^\']*)\'|([^<>\'"\\s]*))[^<>]*>/i', $product["note"], $media);
+                }
+                if ($product["text"]) {
+                    preg_match_all('/<img(?:\\s[^<>]*?)?\\bsrc\\s*=\\s*(?|"([^"]*)"|\'([^\']*)\'|([^<>\'"\\s]*))[^<>]*>/i', $product["text"], $media);
+                }
+                if (!empty($media[1][0])) {
+                    if (strpos($media[1][0], $this->hostname))
+                        $media[1][0] = str_replace("http://" . $this->hostname, "", $media[1][0]);
+                    if ((strpos($media[1][0], "http:") === false) && (strpos($media[1][0], "https:") === false)) {
+                        $fileName = trim(str_replace("/images/{$this->lang}/{$this->section}", "", $media[1][0]), "/");
+                        if (!in_array($fileName, $usedImages))
+                            $usedImages[] = $fileName;
+                    }
+                }
+            }
+
             $u = new DB('shop_img', 'si');
             $u->select('picture name');
             $images = $u->getList();
@@ -101,11 +120,31 @@ class Image extends Base
         if ($this->section == 'shopgroup' && $isUnused) {
 
             $u = new DB('shop_group', 'sg');
-            $u->select('picture name');
-            $images = $u->getList();
-            foreach ($images as $image)
-                if ($image['name'])
-                    $usedImages[] = $image['name'];
+            $u->select('picture name, commentary, footertext');
+            $groups = $u->getList();
+            foreach ($groups as $group) {
+                if ($group['name'])
+                    $usedImages[] = $group['name'];
+
+                $media = [];
+                if ($group["commentary"]) {
+                    preg_match_all('/<img(?:\\s[^<>]*?)?\\bsrc\\s*=\\s*(?|"([^"]*)"|\'([^\']*)\'|([^<>\'"\\s]*))[^<>]*>/i', $group["commentary"], $media);
+                }
+                if ($group["footertext"]) {
+                    preg_match_all('/<img(?:\\s[^<>]*?)?\\bsrc\\s*=\\s*(?|"([^"]*)"|\'([^\']*)\'|([^<>\'"\\s]*))[^<>]*>/i', $group["footertext"], $media);
+                }
+                if (!empty($media[1][0])) {
+                    if (strpos($media[1][0], $this->hostname))
+                        $media[1][0] = str_replace("http://" . $this->hostname, "", $media[1][0]);
+                    if ((strpos($media[1][0], "http:") === false) && (strpos($media[1][0], "https:") === false)) {
+                        $fileName = trim(str_replace("/images/{$this->lang}/{$this->section}", "", $media[1][0]), "/");
+                        if (!in_array($fileName, $usedImages))
+                            $usedImages[] = $fileName;
+                    }
+                }
+
+            }
+
             $u = new DB('shop_img', 'si');
             $u->select('picture name');
             $images = $u->getList();
@@ -129,11 +168,29 @@ class Image extends Base
         }
         if ($this->section== 'shopbrand' && $isUnused) {
             $u = new DB('shop_brand', 'sb');
-            $u->select('image name');
-            $images = $u->getList();
-            foreach ($images as $image)
-                if ($image['name'])
-                    $usedImages[] = $image['name'];
+            $u->select('image name, text, content');
+            $brands = $u->getList();
+            foreach ($brands as $brand) {
+                if ($brand['name'])
+                    $usedImages[] = $brand['name'];
+
+                $media = [];
+                if ($brand["text"]) {
+                    preg_match_all('/<img(?:\\s[^<>]*?)?\\bsrc\\s*=\\s*(?|"([^"]*)"|\'([^\']*)\'|([^<>\'"\\s]*))[^<>]*>/i', $brand["text"], $media);
+                }
+                if ($brand["content"]) {
+                    preg_match_all('/<img(?:\\s[^<>]*?)?\\bsrc\\s*=\\s*(?|"([^"]*)"|\'([^\']*)\'|([^<>\'"\\s]*))[^<>]*>/i', $brand["content"], $media);
+                }
+                if (!empty($media[1][0])) {
+                    if (strpos($media[1][0], $this->hostname))
+                        $media[1][0] = str_replace("http://" . $this->hostname, "", $media[1][0]);
+                    if ((strpos($media[1][0], "http:") === false) && (strpos($media[1][0], "https:") === false)) {
+                        $fileName = trim(str_replace("/images/{$this->lang}/{$this->section}", "", $media[1][0]), "/");
+                        if (!in_array($fileName, $usedImages))
+                            $usedImages[] = $fileName;
+                    }
+                }
+            }
         }
 
         if (!empty($this->section)) {
